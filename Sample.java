30dc51d80e36696214503bee42d51d1148c0b4c3
Ferelyn Mae <132542829+FerelynGabitano@users.noreply.github.com>
2023-09-14 11:15:02 +0800

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,54 @@
+/*package sample;
+import java.util.*;
+public class Sample{
+    public static void main(String[]args){
+        Scanner myObj =new Scanner(System.in);//Create a Scanner Object
+        System.out.print("Enter your name:");
+        String name =myObj.nextLine();//Read user input
+        System.out.println("My name is:"+ name); //Output user input
+        
+    } 
+} */
+/*package sample;
+import java.io.*;
+public class Sample{
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter your name:");
+        String name =br.readLine();
+        System.out.println("My name is "+ name);
+    }
+    }
+}
+*/
+
+/*
+package sample;
+import java.io.*;
+public class Sample {
+    public static void main(String []args) throws IOException{
+        
+        Console c = System.console();
+        System.out.print ("Enter your name:");
+        String name = c.readLine();
+        System.out.println("My name is " + name);
+    }
+}
+*/
+
+/*
+package sample;
+import java.util.*;
+public class Sample{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number:");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println(" The sum of " + num1 +" and " + num2 + " is " + sum );
+        input.close();
+    }
+}
+*/
